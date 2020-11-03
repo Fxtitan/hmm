2d99da66535a0aa35e377712715185a6ea55e862
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-11-03 15:02:40 -0600

    copy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 import { v4 as uuidv4 } from "uuid";
+
 import TodoView from "./components/TodoView";
+
 export default class App extends Component {
   state = {
     todoList: [
@@ -19,75 +21,7 @@ export default class App extends Component {
       },
     ],
     todoValue: "",
-    showErrorMessage: false,
   };
 
-  handleInputChange = (event) => {
-    //console.log(event.target.name, event.target.value);
-
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+  
   };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    let newTodoObj = {
-      id: uuidv4(),
-      todo: this.state.todoValue,
-    };
-
-    let newArray = [...this.state.todoList, newTodoObj];
-    // let newArray = [...this.state.todoList];
-    // newArray.push(newTodoObj);
-
-    this.setState({
-      todoList: newArray,
-      todoValue: "",
-    });
-  };
-
-  // showTodoList = () => {
-  //   return this.state.todoList.map(({ id, todo }) => {
-  //     return <li key={id}>{todo}</li>;
-  //   });
-  // };
-
-  addFunc = () => {
-    console.log("Add Func");
-  };
-
-  render() {
-    const { todoList, showErrorMessage } = this.state;
-
-    return (
-      <div style={{ textAlign: "center" }}>
-        {showErrorMessage ? <div>Please, enter something todo!</div> : null}
-        <input
-          onChange={this.handleInputChange}
-          style={{ marginTop: 20 }}
-          type="text"
-          name="todoValue"
-          value={this.state.todoValue}
-        />{" "}
-        <button onClick={this.handleSubmit}>Add</button>
-        {/* <ul>{this.showTodoList()}</ul> */}
-        {/* <ul style={{ listStyle: "none" }}>
-          {this.state.todoList.map(({ id, todo }) => {
-            return <li key={id}>{todo}</li>;
-          })}
-        </ul> */}
-        <TodoView
-          todoList={todoList}
-          nameString={"Hamster"}
-          age={123}
-          arrayObject={[1, 2, 3]}
-          trueOrFalse={false}
-          addFunc={this.addFunc}
-          obj={{ 1: 1, 2: 2, 3: 3 }}
-        />
-      </div>
-    );
-  }
-}
--- src/components/TodoView.js
+++ src/components/TodoView.js
@@ -1,36 +1,9 @@
 import React from "react";
-import { arrayOf, shape, number, string } from "prop-types";
 
-const TodoView = ({
-  todoList,
-  nameString,
-  age,
-  arrayObject,
-  trueOrFalse,
-  addFunc,
-  obj,
-}) => {
-  console.log(todoList);
-  return (
-    <ul style={{ listStyle: "none" }}>
-      {todoList.map(({ id, todo }) => {
-        return <li key={id}>{todo}</li>;
-      })}
-    </ul>
-  );
-};
+import "./TodoView.css";
 
-// TodoView.propTypes = {
-//   nameString: PropTypes.string.isRequired,
-// };
+const TodoView = ({  }) => {
 
-TodoView.propTypes = {
-  todoList: arrayOf(
-    shape({
-      id: string.isRequired,
-      todo: string.isRequired,
-    })
-  ),
 };
 
 export default TodoView;
